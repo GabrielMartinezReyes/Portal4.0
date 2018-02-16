b7a28afbb0a4bf103f8da0f829f1ad79a74016b4
Gabriel Martinez <gabriel.martinez.reyes@gmail.com>
2018-02-16 12:49:39 +0100

    First Commit Portal 4.0 Beginning

---
+++ Portal4.0/Portal4.0/Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Extensions.swift
+//  Portal4.0
+//
+//  Created by Gabriel Martinez on 2018-02-16.
+//  Copyright © 2018 Gabriel Martinez. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+var width : CGFloat = 0.2
+var height : CGFloat = 1
+var length : CGFloat = 1
+
+func createBox(){
+    
+}
+
